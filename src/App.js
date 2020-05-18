2b0149871c61c298a103f19179c97d06095ac414
Uday Vunnam <uday4vunnam@gmail.com>
2020-05-18 07:09:00 +0530

    component styling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import { ControlledInputDemo } from './controlled-input/demo';
 import { ModalDemo } from './modal/demo';
 import { MultiCheckboxDemo } from './multi-checkbox/demo';
 
-function App() {
+const ComponentBlocks = ({ children }) => {
+  return (
+    <section className='component-blocks'>
+      {React.Children.map(children, (child) => {
+        return (
+          <div>
+            <h2 className='component-title'>{child.props.name}</h2>
+            {child}
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+const App = () => {
   return (
     <div className='app'>
       <h1 className='header'>React Bits</h1>
-      <section className='component-blocks'>
+      <ComponentBlocks>
+        <MultiCheckboxDemo name='Multi Checkbox' />
         <ControlledInputDemo name='Controlled Input' />
         <ModalDemo name='Modal' />
-        <MultiCheckboxDemo name='Multi Checkbox' />
-      </section>
+      </ComponentBlocks>
     </div>
   );
-}
+};
 
 export default App;
